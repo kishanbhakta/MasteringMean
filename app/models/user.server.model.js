52239ecb4f328cbd460f5c0a50f544a44a73200e
Kishan Bhakta <kishan.d.bhakta@gmail.com>
2016-04-11 17:42:21 -0400

    Included Model Validation and Middleware

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -4,9 +4,74 @@ var mongoose = require('mongoose'),
 var UserSchema = new Schema({
   firstName: String,
   lastName: String,
-  email: String,
-  username: String,
-  password: String
+  email: {
+    type: String,
+    index: true,
+    match: /.+\@.+\..+/
+  },
+  username: {
+    type: String,
+    trim: true,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    validate: [
+      function(password) {
+        return password.length >= 6;
+      },
+      'Password should be longer'
+    ]
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  website: {
+    type: String,
+    set: function(url) {
+      if (!url) {
+        return url;
+      } else {
+        if (url.indexOf('http://') !== 0   && url.indexOf('https://') !== 0) {
+          url = 'http://' + url;
+        }
+
+        return url;
+        }
+    }
+  },
+  role: {
+    type: String,
+    enum: ['Admin', 'Owner', 'User']
+  }
+});
+
+UserSchema.virtual('fullName').get(function(){
+  return this.firstName + ' ' + this.lastName;
+}).set(function(fullName) {
+  var splitName = fullName.split(' ');
+  this.firstName = splitName[0] || '';
+  this.lastName = splitName[1] || '';
+});
+
+UserSchema.set('toJSON', { getters: true });
+
+UserSchema.statics.findOneByUsername = function (username, callback) {
+  this.findOne({ username: new RegExp(username, 'i') }, callback);
+};
+
+UserSchema.methods.authenticate = function(password) {
+  return this.password === password;
+};
+
+UserSchema.post('save', function(next) {
+  if(this.isNew) {
+    console.log('A new user was created.');
+  } else {
+    console.log('A user updated is details.');
+  }
 });
 
 mongoose.model('User', UserSchema);
