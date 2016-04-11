efe60d01fa27255b218606c01703cb332485319a
Kishan Bhakta <kishan.d.bhakta@gmail.com>
2016-04-11 18:34:42 -0400

    Implemented the use of User Authentication using Passport

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,11 +1,18 @@
 exports.render = function(req, res) {
-  if (req.session.lastVisit) {
-    console.log(req.session.lastVisit);
-  }
-
-  req.session.lastVisit = new Date();
-
   res.render('index', {
-    title: 'Hello World'
+    title: 'Hello World',
+    userFullName: req.user ? req.user.fullName : ''
   });
 };
+
+// exports.render = function(req, res) {
+//   if (req.session.lastVisit) {
+//     console.log(req.session.lastVisit);
+//   }
+
+//   req.session.lastVisit = new Date();
+
+//   res.render('index', {
+//     title: 'Hello World'
+//   });
+// };
--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose'),
+    crypto = require('crypto'),
     Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
@@ -6,49 +7,38 @@ var UserSchema = new Schema({
   lastName: String,
   email: {
     type: String,
-    index: true,
-    match: /.+\@.+\..+/
+    match: [/.+\@.+\..+/, "Please fill a valid e-mail address"]
   },
   username: {
     type: String,
-    trim: true,
     unique: true,
-    required: true
+    required: 'Username is required',
+    trim: true
   },
   password: {
     type: String,
     validate: [
-      function(password) {
-        return password.length >= 6;
-      },
-      'Password should be longer'
+    function(password) {
+        return password && password.length > 6;
+      }, 'Password should be longer'
     ]
   },
+  salt: {
+    type: String
+  },
+  provider: {
+    type: String,
+    required: 'Provider is required'
+  },
+  providerId: String,
+  providerData: {},
   created: {
     type: Date,
     default: Date.now
-  },
-  website: {
-    type: String,
-    set: function(url) {
-      if (!url) {
-        return url;
-      } else {
-        if (url.indexOf('http://') !== 0   && url.indexOf('https://') !== 0) {
-          url = 'http://' + url;
-        }
-
-        return url;
-        }
-    }
-  },
-  role: {
-    type: String,
-    enum: ['Admin', 'Owner', 'User']
   }
 });
 
-UserSchema.virtual('fullName').get(function(){
+UserSchema.virtual('fullName').get(function() {
   return this.firstName + ' ' + this.lastName;
 }).set(function(fullName) {
   var splitName = fullName.split(' ');
@@ -56,22 +46,45 @@ UserSchema.virtual('fullName').get(function(){
   this.lastName = splitName[1] || '';
 });
 
-UserSchema.set('toJSON', { getters: true });
+UserSchema.pre('save', function(next) {
+  if (this.password) {
+    this.salt = new Buffer(crypto.randomBytes(16).toString('base64'), 'base64');
+    this.password = this.hashPassword(this.password);
+  }
 
-UserSchema.statics.findOneByUsername = function (username, callback) {
-  this.findOne({ username: new RegExp(username, 'i') }, callback);
+  next();
+});
+
+UserSchema.methods.hashPassword = function(password) {
+  return crypto.pbkdf2Sync(password, this.salt, 10000, 64).toString('base64');
 };
 
 UserSchema.methods.authenticate = function(password) {
-  return this.password === password;
+  return this.password === this.hashPassword(password);
 };
 
-UserSchema.post('save', function(next) {
-  if(this.isNew) {
-    console.log('A new user was created.');
-  } else {
-    console.log('A user updated is details.');
-  }
+UserSchema.statics.findUniqueUsername = function(username, suffix, callback) {
+  var _this = this;
+  var possibleUsername = username + (suffix || '');
+
+  _this.findOne({
+    username: possibleUsername
+  }, function(err, user) {
+    if (!err) {
+      if (!user) {
+        callback(possibleUsername);
+      } else {
+        return _this.findUniqueUsername(username, (suffix || 0) + 1, callback);
+      }
+    } else {
+      callback(null);
+    }
+  });
+};
+
+UserSchema.set('toJSON', {
+  getters: true,
+  virtuals: true
 });
 
 mongoose.model('User', UserSchema);
