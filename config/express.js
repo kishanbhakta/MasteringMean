f583c610aede4c596d0e848333e9449f06d1d0bb
Kishan Bhakta <kishan.d.bhakta@gmail.com>
2016-04-11 16:51:17 -0400

    Created user schema and model with CRUD capabilities

--- config/express.js
+++ config/express.js
@@ -1,18 +1,17 @@
-var  config = require('./config'),
-     express = require('express'),
-     morgan = require('morgan'),
-     compress = require('compression'),
-     bodyParser = require('body-parser'),
-     methodOverride = require('method-override'),
-     session = require('express-session');
+var config = require('./config'),
+    express = require('express'),
+    morgan = require('morgan'),
+    compress = require('compression'),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override'),
+    session = require('express-session');
 
 module.exports = function() {
   var app = express();
 
-
   if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
-  }else if (process.env.NODE_ENV === 'production') {
+  } else if (process.env.NODE_ENV === 'production') {
     app.use(compress());
   }
 
@@ -31,11 +30,10 @@ module.exports = function() {
   app.set('views', './app/views');
   app.set('view engine', 'ejs');
 
-
   require('../app/routes/index.server.routes.js')(app);
+  require('../app/routes/users.server.routes.js')(app);
 
-  app.use(express.static('./public')); //Serve static files
-
+  app.use(express.static('./public'));
 
   return app;
 };
