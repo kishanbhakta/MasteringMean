0678351c7f194d0f7d092b35c78f5aeaf3309daa
Kishan Bhakta <kishan.d.bhakta@gmail.com>
2016-04-15 00:51:18 -0400

    resolved OAuth error

--- config/env/development.js
+++ config/env/development.js
@@ -2,7 +2,7 @@ module.exports = {
   db: 'mongodb://localhost/mean-book',
   sessionSecret: 'developmentSessionSecret',
   facebook: {
-    clientId: '698177743653545',
+    clientID: '698177743653545',
     clientSecret: '8440ba1f85b6d905ff84be1912172e5c',
     callbackURL: 'http://localhost:3000/oauth/facebook/callback'
   },
