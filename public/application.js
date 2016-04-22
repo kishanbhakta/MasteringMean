54183a2709a5cdc07cc1318012980e1506dfed8e
Kishan Bhakta <kishan.d.bhakta@gmail.com>
2016-04-21 21:25:43 -0400

    Completed adding real-time functionality using socket.io and built a chat application

--- public/application.js
+++ public/application.js
@@ -1,6 +1,6 @@
 var mainApplicationModuleName = 'mean';
 
-var mainApplicationModule = angular.module(mainApplicationModuleName, ['ngResource', 'ngRoute', 'users', 'example', 'articles']);
+var mainApplicationModule = angular.module(mainApplicationModuleName, ['ngResource', 'ngRoute', 'users', 'example', 'articles', 'chat']);
 
 mainApplicationModule.config(['$locationProvider',
   function($locationProvider) {
@@ -9,6 +9,7 @@ mainApplicationModule.config(['$locationProvider',
 ]);
 
 if (window.location.hash === '#_=_') window.location.hash = '#!';
+
 angular.element(document).ready(function() {
   angular.bootstrap(document, [mainApplicationModuleName]);
 });
